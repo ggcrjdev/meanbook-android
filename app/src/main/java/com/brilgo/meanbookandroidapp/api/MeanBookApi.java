db4ca606be222999836ecceaa12d8016dafd5aa7
gustavo.gomes <gustavo.gomesc@gmail.com>
2016-11-17 16:10:35 -0200

    Added some methods to the API.

--- app/src/main/java/com/brilgo/meanbookandroidapp/api/MeanBookApi.java
+++ app/src/main/java/com/brilgo/meanbookandroidapp/api/MeanBookApi.java
@@ -2,10 +2,12 @@ package com.brilgo.meanbookandroidapp.api;
 
 import android.util.Log;
 
+import com.brilgo.meanbookandroidapp.api.request.PostsAddRequest;
 import com.brilgo.meanbookandroidapp.api.request.UsernameRequest;
 import com.brilgo.meanbookandroidapp.api.response.Post;
 import com.brilgo.meanbookandroidapp.api.response.PostsListResponse;
 import com.brilgo.meanbookandroidapp.api.response.User;
+import com.brilgo.meanbookandroidapp.api.response.UsersListResponse;
 import com.brilgo.meanbookandroidapp.api.response.UsersLogoutResponse;
 
 import java.io.IOException;
@@ -22,16 +24,16 @@ public class MeanBookApi {
     private static final String BASE_URL_API = "http://10.0.2.2:3000/api/1.0/";
 
     private static Retrofit retrofit;
-    private static MeanBookApiClient meanBookApiClient;
+    private static MeanBookApiEndpoint meanBookApiEndpoint;
 
     public MeanBookApi() {
         init();
     }
 
     public static void init() {
-        if (meanBookApiClient == null) {
+        if (meanBookApiEndpoint == null) {
             createRetrofit();
-            meanBookApiClient = retrofit.create(MeanBookApiClient.class);
+            meanBookApiEndpoint = retrofit.create(MeanBookApiEndpoint.class);
         }
     }
 
@@ -54,18 +56,27 @@ public class MeanBookApi {
 
     public User login(String username) {
         UsernameRequest request = new UsernameRequest(username);
-        User response = executeRequest(meanBookApiClient.login(request));
+        User response = executeRequest(meanBookApiEndpoint.login(request));
         return response;
     }
 
     public boolean logout(String username) {
         UsernameRequest request = new UsernameRequest(username);
-        UsersLogoutResponse response = executeRequest(meanBookApiClient.logout(request));
+        UsersLogoutResponse response = executeRequest(meanBookApiEndpoint.logout(request));
         return response.logggedOut;
     }
 
+    public List<User> listOnlineUsers() {
+        UsersListResponse response = executeRequest(meanBookApiEndpoint.listUsers());
+        if (response != null) {
+            return response.users;
+        } else {
+            return new ArrayList<>(0);
+        }
+    }
+
     public List<Post> listPosts(String username) {
-        PostsListResponse response = executeRequest(meanBookApiClient.listPosts(username, 1));
+        PostsListResponse response = executeRequest(meanBookApiEndpoint.listPosts(username, 1));
         if (response != null) {
             Log.d(getClass().getName(), MessageFormat.format(
                     "Retrieving {0} posts included by the {1} user.",
@@ -74,5 +85,11 @@ public class MeanBookApi {
         } else {
             return new ArrayList<>(0);
         }
+    }
+
+    public void addPost(String text) {
+        PostsAddRequest request = new PostsAddRequest();
+        request.setText(text);
+        executeRequest(meanBookApiEndpoint.addPost(request));
     }
 }
--- app/src/main/java/com/brilgo/meanbookandroidapp/api/MeanBookApiEndpoint.java
+++ app/src/main/java/com/brilgo/meanbookandroidapp/api/MeanBookApiEndpoint.java
@@ -1,6 +1,8 @@
 package com.brilgo.meanbookandroidapp.api;
 
+import com.brilgo.meanbookandroidapp.api.request.PostsAddRequest;
 import com.brilgo.meanbookandroidapp.api.request.UsernameRequest;
+import com.brilgo.meanbookandroidapp.api.response.PostsAddResponse;
 import com.brilgo.meanbookandroidapp.api.response.PostsListResponse;
 import com.brilgo.meanbookandroidapp.api.response.User;
 import com.brilgo.meanbookandroidapp.api.response.UsersCurrentResponse;
@@ -13,13 +15,7 @@ import retrofit2.http.GET;
 import retrofit2.http.POST;
 import retrofit2.http.Path;
 
-public interface MeanBookApiClient {
-
-    @GET("users/list")
-    Call<UsersListResponse> list();
-
-    @POST("users/current")
-    Call<UsersCurrentResponse> getCurrent();
+public interface MeanBookApiEndpoint {
 
     @POST("users/login")
     Call<User> login(@Body UsernameRequest request);
@@ -27,8 +23,17 @@ public interface MeanBookApiClient {
     @POST("users/logout")
     Call<UsersLogoutResponse> logout(@Body UsernameRequest request);
 
+    @GET("users/list")
+    Call<UsersListResponse> listUsers();
+
+    @POST("users/current")
+    Call<UsersCurrentResponse> getCurrent();
+
     @GET("posts/list/{username}/{pageNumber}")
     Call<PostsListResponse> listPosts(
             @Path("username") String username,
             @Path("pageNumber") Integer pageNumber);
+
+    @POST("posts/add")
+    Call<PostsAddResponse> addPost(@Body PostsAddRequest request);
 }
