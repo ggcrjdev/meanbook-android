fa79e0d7261efbbca36e35fcd5cf814f6a4e1795
gustavo.gomes <gustavo.gomesc@gmail.com>
2016-12-03 01:36:25 -0200

    Improved the runtime exception handling.

--- app/src/main/java/com/brilgo/meanbookandroidapp/ExceptionHandler.java
+++ app/src/main/java/com/brilgo/meanbookandroidapp/ExceptionHandler.java
@@ -6,6 +6,10 @@ import android.util.Log;
 
 import com.brilgo.meanbookandroidapp.api.RequestApiException;
 
+import java.lang.reflect.InvocationTargetException;
+import java.util.ArrayList;
+import java.util.List;
+
 class ExceptionHandler implements Thread.UncaughtExceptionHandler {
 
     private static final String TAG = ExceptionHandler.class.getSimpleName();
@@ -22,7 +26,9 @@ class ExceptionHandler implements Thread.UncaughtExceptionHandler {
 
     @Override
     public void uncaughtException(final Thread thread, final Throwable ex) {
-        if (ex instanceof RequestApiException || ex.getCause() instanceof RequestApiException) {
+        List<String> acceptedExceptions = new ArrayList<>();
+        acceptedExceptions.add(RequestApiException.class.getName());
+        if (acceptToHandleException(ex, acceptedExceptions)) {
             Log.e(TAG, ex.getMessage(), ex);
 
             Intent registerActivity = new Intent(context, ApiActivity.class);
@@ -35,5 +41,21 @@ class ExceptionHandler implements Thread.UncaughtExceptionHandler {
         } else {
             rootHandler.uncaughtException(thread, ex);
         }
+    }
+
+    private boolean acceptToHandleException(Throwable ex, List<String> acceptedExceptions) {
+        boolean handleException = false;
+        Throwable nextEx = ex;
+        while (nextEx != null) {
+            if (acceptedExceptions.contains(nextEx.getClass().getName())) {
+                handleException = true;
+            }
+            if (nextEx instanceof InvocationTargetException) {
+                nextEx = ((InvocationTargetException) nextEx).getTargetException();
+            } else {
+                nextEx = nextEx.getCause();
+            }
+        }
+        return handleException;
     }
 }
